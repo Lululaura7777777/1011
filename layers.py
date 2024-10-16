077ff385742b7deda0773b9547410d58c3e5e096
Lululaura7777777 <161525041+Lululaura7777777@users.noreply.github.com>
2024-10-15 22:09:20 -0400

    Update layers.py

--- layers.py
+++ layers.py
@@ -127,7 +127,7 @@ class MultiHeadedAttention(nn.Module):
         context = context.transpose(1, 2).contiguous().view(batch_size, -1, self.h * self.d_v)
         output = self.linear(context)
         
-        return output, attn
+        return output
 
 
     
